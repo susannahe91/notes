0c78aa5bd29588a09b884b65e3f05797a673e83e
susannahe91 <56616510+susannahe91@users.noreply.github.com>
2022-06-22 16:38:04 -0700

    deploy

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+wk&r*dehh-=gbgq1@wsj6nx891o=!$p%=2d^myo!j=xvdtm&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
